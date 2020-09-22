23cdcd0c6f13cc6d9fb7e814d7cc2633edd8b104
Vivian <50971283+vsysms@users.noreply.github.com>
2020-09-22 15:51:11 -0400

    Update thread.c
    Test to see if macro appears in new branch

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -20,6 +20,9 @@
    of thread.h for details. */
 #define THREAD_MAGIC 0xcd6abf4b
 
+/* Macro used for fixed point arithmetic. */
+#define F 16384
+
 /* List of processes in THREAD_READY state, that is, processes
    that are ready to run but not actually running. */
 static struct list ready_list;
