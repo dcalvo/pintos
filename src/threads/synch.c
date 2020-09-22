ded825b16ce1a7fbcec769ecaf374b9bf8244d30
David Calvo <dcalvo2@jhu.edu>
2020-09-22 04:46:51 -0400

    add priority queue to waiters

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      push_waiting_thread (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
   sema->value--;
@@ -295,7 +295,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  push_waiting_thread (&cond->waiters, &waiter.elem);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -354,5 +354,5 @@ priority_greater_comp (const struct list_elem *t1, const struct list_elem *t2, v
 void
 push_waiting_thread (struct list *waiters, struct thread *t)
 {
-  list_insert_ordered (&waiters, &t->elem, &priority_greater_comp, NULL);
+  list_insert_ordered (waiters, &t->elem, &priority_greater_comp, NULL);
 }
