c753e616f1a471cdab50e973d88f5d46a1b17673
David Calvo <dcalvo2@jhu.edu>
2020-10-14 23:38:29 -0400

    add access checks

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,7 @@
 #include "filesys/filesys.h"
 #include "threads/palloc.h"
 #include "devices/shutdown.h"
+#include "vaddr.h"
 
 struct fd
   {
@@ -14,6 +15,7 @@ struct fd
     struct list_elem elem;
   };
 
+static void memfetch (void *dest, void *src, size_t size);
 static void syscall_handler (struct intr_frame *);
 static void open (struct intr_frame *);
 static void sys_write (struct intr_frame *);
@@ -74,14 +76,62 @@ static void
 sys_write (struct intr_frame *f)
 {
   int wrote = 0;
-  int fd = *(int *)(f->esp + 4);
-  const void *buffer = (f->esp + 8);
-  unsigned size = *(int *)(f->esp + 12);
+  int fd, size;
+  const void *buf;
+
+  memfetch (&fd, f->esp + 4, sizeof (fd));
+  memfetch (&buf, f->esp + 8, sizeof (buf));
+  memfetch (&size, f->esp + 12, sizeof (size));
 
   if (fd == 1) {
-    putbuf(buffer, size);
-    wrote = size;
+    putbuf(buf, size);
   }
+}
 
-  return wrote;
+/* Safely fetch register value from SRC and store it into DEST. Reads up to SIZE bytes. */
+static void
+memfetch (void *dest, void *src, size_t size)
+{
+  int val;
+  for (size_t i = 0; i < size; i++)
+  {
+    val = get_user (src + i);
+    if (val == -1)
+    {
+      PANIC("implement unsafe access error handling");
+    }
+
+    *((char *)(dest + i)) = val & 0xff;
+  }
+}
+
+/* From the project page. */
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a segfault
+   occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  if (PHYS_BASE > (void *) uaddr) return -1;
+
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/* From the project page. */
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  if (PHYS_BASE > (void *) udst) return false;
+  
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
 }
