3cb634fd66e43436e6c1f908b50a2bef5d59c679
David Calvo <dcalvo2@jhu.edu>
2020-11-11 13:03:17 -0500

    update header

--- src/vm/page.h
+++ src/vm/page.h
@@ -1,6 +1,9 @@
 #include <hash.h>
 
 bool page_load (void *fault_addr);
+unsigned page_hash (const struct hash_elem *p_, void *aux UNUSED);
+bool page_less (const struct hash_elem *a_, const struct hash_elem *b_,
+           void *aux UNUSED);
 
 struct page
 {
