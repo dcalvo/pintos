799e2d7ab60ea3e28f064b20a2ebd32d41c3e371
David Calvo <dcalvo2@jhu.edu>
2020-11-11 18:49:33 -0500

    test debugging compare

--- src/vm/page.c
+++ src/vm/page.c
@@ -89,7 +89,7 @@ page_get (void *address)
     else {
         /* Checking that the page address is inside max stack size
          and at most 32 bytes away. */
-        if (pte.addr > PHYS_BASE - USER_STACK && 
+        if (pte.addr > PHYS_BASE - USER_STACK || 
             address > t->esp - 32)
             return page_alloc (pte.addr, true);
     }
